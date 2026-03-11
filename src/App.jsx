6d74e9df0a81b42e17cb4d4badf305cb1d2bedc6
BhupatiNadar <tamillana2@gmail.com>
2026-03-11 18:07:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Homepage/Navbar"
+import BodyContent from "./components/Homepage/BodyContent";
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <BodyContent />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Homepage/Navbar.jsx
@@ -0,0 +1,31 @@
+import "./Navbar.css"
+
+import icon from "../../assets/database_icon.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-icon_and_project_name">
+        <img
+          src={icon}
+          alt="database icon"
+          className="navbar-icon"
+          style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+        />
+        <h2>AI SQL Chatbot</h2>
+      </div>
+      <div className="navbar-main_navbar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Features</li>
+          <li>How it works</li>
+          <li className="navbar-chatbot-btn">chatbot</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
